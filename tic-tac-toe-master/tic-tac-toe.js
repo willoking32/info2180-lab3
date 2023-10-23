40b2ab3dabfbabc40ea9859c034b787317abf6eb
William Whitelocke <wewhitelocke@gmail.com>
2023-10-22 19:26:25 -0500

    Complete and working JS code

--- tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe.js
@@ -2,9 +2,17 @@
 document.addEventListener('DOMContentLoaded',() =>{
 	let board = document.getElementById('board');
 	let boxes = board.childNodes;
+	//console.log(boxes);
 	let newgame = document.querySelector('.btn');
 	//console.log(newgame);
 	
+	//tracking
+	let trace = ["","","","","","","","",""];
+	let turn = 0;
+	let wincon = document.getElementById('status');
+	
+	
+
 	for(var i = 1; i < boxes.length; i += 2){
         boxes[i].classList.add("square");
     };
@@ -22,19 +30,75 @@ boxes.forEach(function(elem, index) {
 	});
 	
 	boxes.forEach(function(elem, index) {
+		
+		
+		elem.addEventListener('click',function(e){
+			let xtext = document.createTextNode('X');
+			let otext = document.createTextNode('O');
+			
+			let a = (index / 2)+1 - 0.5;
+			
+			//console.log(index);
+			if (trace[a-1] == ""){
+				if (turn % 2 == 0){
+					elem.appendChild(otext);
+					e.target.classList.add('square.O');
+					trace[a-1] = "O";
+					
+				}
+				else{
+					elem.appendChild(xtext);
+					e.target.classList.add('square.X');
+					trace[a-1] = "X";
+				}
+				console.log(a);
+				turn += 1;
+				//console.log(turn);
+				console.log(trace);
+			}
+			//console.log(board);
+		});
+		
+		//mouse on
 		elem.addEventListener('mouseover', function(e) {
 			e.target.classList.add('hover');
+			//console.log(boxes);
 		});
+		
+		//mouse off
 		elem.addEventListener('mouseout', function(e) {
 			e.target.classList.remove('hover');
 		});
 	});
 	
+	
+	boxes.forEach(function(elem, index) {
+		
+		
+		elem.addEventListener('click',() =>{
+			for (var i = 1;i<trace.length;i +=3){
+				if (trace[i] !== ""){
+					if(trace[i-1] === trace[i] && trace[i] === trace[i+1]){
+						wincon.textContent = 'Congratulations! '+ trace[i]+ ' is the Winner!';
+						console.log(wincon);
+					}
+				}
+			}
+			//console.log(trace.legnth);
+			for (var i = 0; i<= 3; i++){
+				if (trace[i] !== ""){
+					if ((trace[i] === trace[i+3] && trace[i+3] === trace[i+6]) || (trace[i] === trace[4] && trace[4] === trace[8 - i])){
+						wincon.textContent = 'Congratulations! '+ trace[i]+ ' is the Winner!';
+						//console.log(wincon);
+					}
+				}
+			}
+		});
+	});
+	
+	
+	
+	newgame.addEventListener('click',()=> {
+		 location. reload(true);
+	});
 });
-
-
-// start game
-
-
-
-
